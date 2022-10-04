c3a6f3c77b5203be822d6661c3956efcbc481161
bilguun70b1123@gmail.com <bilguun70b1123@gmail.com>
2022-10-04 17:59:52 +0800

    Changed Navbar

--- front-end/components/LogIn/LogIn.jsx
+++ front-end/components/LogIn/LogIn.jsx
@@ -1,90 +1,82 @@
-import * as React from 'react';
-import Avatar from '@mui/material/Avatar';
-import Button from '@mui/material/Button';
-import CssBaseline from '@mui/material/CssBaseline';
-import TextField from '@mui/material/TextField';
-import FormControlLabel from '@mui/material/FormControlLabel';
-import Checkbox from '@mui/material/Checkbox';
-import Link from '@mui/material/Link';
-import Grid from '@mui/material/Grid';
-import Box from '@mui/material/Box';
-import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
-import Typography from '@mui/material/Typography';
-import Container from '@mui/material/Container';
-
+import Box from "@mui/material/Box";
+import Link from "@mui/material/Link";
+import Grid from "@mui/material/Grid";
+import Button from "@mui/material/Button";
+import TextField from "@mui/material/TextField";
+import Container from "@mui/material/Container";
+import Typography from "@mui/material/Typography";
+import CssBaseline from "@mui/material/CssBaseline";
 
 const LogIn = () => {
   const handleSubmit = (event) => {
     event.preventDefault();
     const data = new FormData(event.currentTarget);
     console.log({
-      email: data.get('email'),
-      password: data.get('password'),
+      email: data.get("email"),
+      password: data.get("password"),
     });
   };
 
   return (
-      <Container component="main" maxWidth="xs">
-        <CssBaseline />
-        <Box
-          sx={{
-            marginTop: 18,
-            display: 'flex',
-            flexDirection: 'column',
-            alignItems: 'center',
-          }}
-        >
-          <Typography component="h1" variant="h5" sx={{ color: "#000" }}>
-            Log In
-          </Typography>
-          <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
-            <TextField
-              margin="normal"
-              required
-              fullWidth
-              id="email"
-              label="Email Address"
-              name="email"
-              autoComplete="email"
-              autoFocus
-            />
-            <TextField
-              margin="normal"
-              required
-              fullWidth
-              name="password"
-              label="Password"
-              type="password"
-              id="password"
-              autoComplete="current-password"
-            />
-            <Button
-              type="submit"
-              fullWidth
-              variant="contained" 
-              sx={{ mt: 3, mb: 2, backgroundColor: "#000" }}
-            >
-              <Link href='/detail'>
-                Log In
+    <Container component="main" maxWidth="xs">
+      <CssBaseline />
+      <Box
+        sx={{
+          marginTop: 18,
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+        }}
+      >
+        <Typography component="h1" variant="h5" sx={{ color: "#000" }}>
+          Log In
+        </Typography>
+        <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
+          <TextField
+            margin="normal"
+            required
+            fullWidth
+            id="email"
+            label="Email Address"
+            name="email"
+            autoComplete="email"
+            autoFocus
+          />
+          <TextField
+            margin="normal"
+            required
+            fullWidth
+            name="password"
+            label="Password"
+            type="password"
+            id="password"
+            autoComplete="current-password"
+          />
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            sx={{ mt: 3, mb: 2, backgroundColor: "#000" }}
+          >
+            <Link href="/detail">Log In</Link>
+            {/* Log In */}
+          </Button>
+          <Grid container>
+            <Grid item xs>
+              <Link href="#" variant="body2">
+                Forgot password?
               </Link>
-              {/* Log In */}
-            </Button>
-            <Grid container>
-              <Grid item xs>
-                <Link href="#" variant="body2">
-                  Forgot password?
-                </Link>
-              </Grid>
-              <Grid item>
-                <Link href="/signUp" variant="body2">
-                  {"Don't have an account? Sign Up"}
-                </Link>
-              </Grid>
             </Grid>
-          </Box>
+            <Grid item>
+              <Link href="/signUp" variant="body2">
+                {"Don't have an account? Sign Up"}
+              </Link>
+            </Grid>
+          </Grid>
         </Box>
-      </Container>
+      </Box>
+    </Container>
   );
-}
+};
 
 export default LogIn;
--- front-end/components/Navbar/NavbarStyle.js
+++ front-end/components/Navbar/NavbarStyle.js
@@ -1,38 +1,61 @@
 export const styles = {
-    navbarSection: theme => ({
-        top: "0%",
-        zIndex: 10,
-        height: "10vh",
-        width: "100vw", 
-        display: "flex", 
-        position: "fixed",
-        alignItems: "center", 
-        backgroundColor: "#000",
-        justifyContent: "space-between",
-        padding: "0 140px", 
-        [theme.breakpoints.down("md")]: {
-            padding: "0 3em",
-        },
-    }),
-    navbarSectionTrans: theme => ({
-        top: "0%",
-        zIndex: 10,
-        height: "10vh",
-        width: "100vw", 
-        display: "flex", 
-        position: "fixed",
-        alignItems: "center", 
-        backgroundColor: "transparent",
-        justifyContent: "space-between",
-        padding: "0 140px", 
-        [theme.breakpoints.down("md")]: {
-            padding: "0 3em",
-        },
-    }),
-    btns: theme => ({
-        display: "flex", 
-        [theme.breakpoints.down("700px")]: {
-            
-        }
-    })
-}
+  navbarSection: (theme) => ({
+    top: "0%",
+    zIndex: 10,
+    height: "10vh",
+    width: "100vw",
+    display: "flex",
+    position: "fixed",
+    alignItems: "center",
+    backgroundColor: "#000",
+    justifyContent: "space-between",
+    padding: "0 140px",
+    [theme.breakpoints.down("md")]: {
+      padding: "0 3em",
+    },
+  }),
+  navbarSectionTrans: (theme) => ({
+    top: "0%",
+    zIndex: 10,
+    height: "10vh",
+    width: "100vw",
+    display: "flex",
+    position: "fixed",
+    alignItems: "center",
+    backgroundColor: "transparent",
+    justifyContent: "space-between",
+    padding: "0 140px",
+    [theme.breakpoints.down("md")]: {
+      padding: "0 3em",
+    },
+  }),
+  btns: (theme) => ({
+    display: "flex",
+    [theme.breakpoints.down(700)]: {
+      display: "none",
+    },
+  }),
+  bar: (theme) => ({
+    display: "none",
+    [theme.breakpoints.down(700)]: {
+      display: "block",
+    },
+  }),
+  SecondBtns: (theme) => ({
+    top: "0",
+    left: "0",
+    right: "0",
+    bottom: "0",
+    width: "100vw",
+    height: "100vh",
+    display: "flex",
+    position: "fixed",
+    alignItems: "center",
+    flexDirection: "column",
+    backgroundColor: "black",
+    justifyContent: "center",
+  }),
+  SecondBtnsNone: (theme) => ({
+    display: "none",
+  }),
+};
